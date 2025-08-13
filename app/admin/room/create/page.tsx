f6f4234701b0a58f14efc2078b98a47b22553e06
Rezza Agung Pambudi <141853553+rezza-pambudi@users.noreply.github.com>
2025-08-13 18:30:18 +0700

    create room

--- app/admin/room/create/page.tsx
+++ app/admin/room/create/page.tsx
@@ -1,7 +1,8 @@
+import CreateRoom from "@/components/admin/room/create-room"
 const CreateRoomPage = () => {
   return (
     <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
-      
+      <CreateRoom />
     </div>
   )
 }
--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+"use client";
+
+import { useRef, useState } from 'react';
+import {IoCloudUploadOutline} from 'react-icons/io5';
+import {type PutBlobResult } from '@vercel/blob';
 
 const CreateForm = () => {
+const inputFileRef = useRef<HTMLInputElement>(null);
+const [image, setImage] = useState('');
+const [message, setMessage] = useState('');
+
+const handleUpload = async() => {
+    if (!inputFileRef.current?.files) return null;
+    const file = inputFileRef.current.files[0];
+    const formData = new FormData();
+    formData.set('file', file);
+
+    try {
+        const response = await fetch('/api/upload', {
+            method: 'PUT',
+            body: formData,
+        })
+        const data = await response.json();
+        if(response.status !== 200) {
+            setMessage(data.message);
+        }
+        const img = data as PutBlobResult;
+        
+    } catch (error) {
+        
+    }
+}
   return (
     <form action="">
         <div className='grid md:grid-cols-12 gap-5'>
@@ -17,8 +46,41 @@ const CreateForm = () => {
                         <span className='text-sm text-red-500 mt:2'>Message</span>
                     </div>
                 </div>
+                <div className='mb-4 grid md:grid-cols-3 '>
+                    <input type="checkbox" name='amenities' className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded'/>
+                    <label className='ms-2 text-sm font-medium text-gray-900 capitalize'>
+                        Spa
+                    </label>
+                    <div aria-live='polite' aria-atomic='true'>
+                        <span className='text-sm text-red-500 mt:2'>Message</span>
+                    </div>
+                </div>
             </div>
-            <div className='col-span-4 bg-white p-4'></div>
+            <div className='col-span-4 bg-white p-4'>
+                <label htmlFor="input-file" className='flex flex-col mb-4 items-center justify-center aspect-video border-2 border-gray-300 border-dashed rounded-md cursor-pointer bg-gray-50 relative'>
+                    <div className='flex flex-col items-center justify-center text-gray-500 pt-5 pb-6 z-10'>
+                        <div className='flex flex-col items-center justify-center'>
+                            <IoCloudUploadOutline className='size-8' />
+                            <p className='mb-1 text-sm font-bold'>Select Image</p>
+                            <p className='text-xs'>SVG, PNG, JPG or Other (Max: 4mb)</p>
+                        </div>
+                    </div>
+                    <input id="input-file" ref={inputFileRef} type="file" className='hidden' />
+                </label>
+                <div className='mb-4'>
+                    <input type="text" name='capacity' className='py-2 px-4 rounded-sm border border-gray-400 w-full' placeholder='Capacity..' />
+                    <div aria-live='polite' aria-atomic='true'>
+                        <span className='text-sm text-red-500 mt:2'>Message</span>
+                    </div>
+                </div>
+                <div className='mb-4'>
+                    <input type="text" name='price' className='py-2 px-4 rounded-sm border border-gray-400 w-full' placeholder='Price...' />
+                    <div aria-live='polite' aria-atomic='true'>
+                        <span className='text-sm text-red-500 mt:2'>Message</span>
+                    </div>
+                </div>
+                <button type='submit' className='bg-orange-400 text-white w-full hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold cursor-pointer'>Save</button>
+            </div>
         </div>
     </form>
   )
